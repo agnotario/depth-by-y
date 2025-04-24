eb37b2604c3a5e2f1711546ce5a71728734b08c6
agnotario <agnotario@gmail.com>
2025-04-24 11:13:58 +0200

    Icon changed

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-//% weight=100 color=#0fbc11 icon="clone"
-//% block="profundidad Y"
+//% weight=100 color=#0fbc11 icon="\uf87f"
+//% block="Depth By Y"
 namespace depthByY {
     let spritesToSort: Sprite[] = []
 
