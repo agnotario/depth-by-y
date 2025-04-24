577a494f8b6a646030db64eb0e1bdb3f71b5459c
agnotario <agnotario@gmail.com>
2025-04-24 10:38:05 +0200

    First Version of Blocks

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
+//% weight=100 color=#0fbc11 icon="⬇️"
+//% block="profundidad Y"
+namespace depthByY {
+    let spritesToSort: Sprite[] = []
 
+    /**
+     * Registra un sprite para ordenarse automáticamente por su posición Y.
+     * @param sprite el sprite a ordenar
+     */
+    //% block="ordenar sprite $sprite por Y"
+    export function addSprite(sprite: Sprite) {
+        if (spritesToSort.indexOf(sprite) === -1) {
+            spritesToSort.push(sprite)
+        }
+    }
+
+    game.onUpdate(function () {
+        spritesToSort.sort((a, b) => a.y - b.y)
+        spritesToSort.forEach((sprite, index) => {
+            sprite.z = index
+        })
+    })
+}
